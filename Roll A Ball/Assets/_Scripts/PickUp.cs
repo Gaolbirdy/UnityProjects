56d1b0d3712bd24d5eaed2c5ee5f8a24a6b8d634
Gaolbirdy <gaolbird@yeah.net>
2017-10-01 12:08:01 +0800

    Initial commit

---
+++ Roll A Ball/Assets/_Scripts/PickUp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour 
+{
+
+	void Start () 
+	{
+		
+	}
+	
+	void Update () 
+	{
+        transform.Rotate(new Vector3(0, 1, 0));
+	}
+}
---
+++ Roll A Ball/Assets/_Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour 
+{
+    public Rigidbody rb;
+    public int force = 5;
+    public Text text;
+    public GameObject winText;
+
+    private int score = 0;
+
+    void Start () 
+	{
+        // 可以在检视面板把该GameObject赋给到自己脚本的刚体字段，前提是带有刚体组件
+        //rb = GetComponent<Rigidbody>();
+        text.text = score.ToString();
+    }
+	
+	void FixedUpdate () 
+	{
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        rb.AddForce(new Vector3(h, 0, v) * force);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //string name = collision.collider.name;
+        //print(name);
+        if(collision.collider.tag == "PickUp")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "PickUp")
+        {
+            score++;
+            text.text = score.ToString();
+            transform.localScale *= (1 + score * 0.05f);
+            if(score == 8)
+            {
+                winText.SetActive(true);
+            }
+            Destroy(other.gameObject);
+        }
+    }
+}
