e7dd0fcdf314173b77da636ac8384516da4d0eb8
Gaolbirdy <gaolbird@yeah.net>
2017-10-01 12:09:19 +0800

    Test

---
+++ BallGame/Assets/BallController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+ {
+    public float thrust = 40.0f;
+    private Rigidbody rb;
+
+    void Start () 
+	{
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if(transform.position.y < -10)
+        {
+            Destroy(gameObject);
+            Application.Quit();
+        }
+    }
+
+    void FixedUpdate () 
+	{
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        rb.AddForce(new Vector3(Random.Range(-0.2f, 0.2f), 1.0f, 0) * thrust);
+    }
+}
---
+++ BallGame/Assets/MovePlate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlate : MonoBehaviour
+ {
+    public float speed = 5.0f;
+
+    void Update () 
+	{
+        float h = Input.GetAxis("Horizontal");
+        // 直接位移， Vector3.right * h * speed 作为每秒位移量
+        transform.Translate(Vector3.right * h * speed * Time.deltaTime);
+    }
+}
