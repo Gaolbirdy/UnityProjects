bb95d6a4b70a53d2e3c9d9cdcbf96c5a269115e7
liukuo <liukuo@gitlab.com>
2017-10-17 07:56:19 +0800

    commit

---
+++ Bricks/Assets/Scripts/Movement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour 
+{
+    public float speed = 3;
+
+    private void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(h, v, 0) * Time.deltaTime * speed);
+    }
+}
---
+++ Bricks/Assets/Scripts/Shoot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour 
+{
+    public GameObject bullet;
+    public float speed = 5;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject b = Instantiate(bullet, transform.position, transform.rotation);
+            Rigidbody rgb = b.GetComponent<Rigidbody>();
+            rgb.velocity = transform.forward * speed;
+        }
+    }
+}
