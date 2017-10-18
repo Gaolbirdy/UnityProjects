d0dd895ce9437a16dd334a1be83f18a99be6df86
liukuo <liukuo@gitlab.com>
2017-10-18 08:29:06 +0800

    commit

---
+++ StickPin/Assets/Scripts/GameController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour 
+{
+    public Transform startPoint;
+    public Transform spawnPoint;
+    public GameObject pinPrefab;
+
+    private Pin currentPin;
+    private bool isGameOver = false;
+
+    private void Start()
+    {
+        SpawnPin();
+    }
+
+    private void Update()
+    {
+        if (isGameOver)
+            return;
+        if (Input.GetMouseButtonDown(0))
+        {
+            currentPin.StartFly();
+            SpawnPin();
+        }       
+    }
+
+    private void SpawnPin()
+    {
+        currentPin = Instantiate(pinPrefab, spawnPoint.position, pinPrefab.transform.rotation).GetComponent<Pin>();
+    }
+
+    public void GameOver()
+    {
+        if (isGameOver)
+            return;
+        GameObject.Find("Circle").GetComponent<RotateSelf>().enabled = false;
+        isGameOver = true;
+    }
+}
---
+++ StickPin/Assets/Scripts/Pin.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour 
+{
+    public float speed = 5;
+    public float stickPosY = 0.7f;
+
+    private bool isFly = false;
+    private bool isReach = false;
+    private Transform startPoint;
+    private Transform circle;
+    private Vector3 targetCirclePos;
+
+    private void Start()
+    {
+        startPoint = GameObject.Find("StartPoint").transform;
+        circle = GameObject.Find("Circle").transform;
+        //circle = GameObject.FindGameObjectWithTag("Circle").transform;
+        targetCirclePos = circle.position;
+        targetCirclePos.y -= stickPosY;
+    }
+
+    private void Update()
+    {
+        if (isFly == false)
+        {
+            if (isReach == false)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, startPoint.position, speed * Time.deltaTime);
+                if (Vector3.Distance(transform.position, startPoint.position) < 0.05f)
+                    isReach = true;
+            }
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetCirclePos, speed * Time.deltaTime);
+            if (Vector3.Distance(transform.position, targetCirclePos) < 0.05f)
+            {
+                transform.position = targetCirclePos;
+                transform.parent = circle;
+                isFly = false;
+            }
+        }
+    }
+
+    public void StartFly()
+    {
+        isFly = true;
+        isReach = true;
+    }
+}
