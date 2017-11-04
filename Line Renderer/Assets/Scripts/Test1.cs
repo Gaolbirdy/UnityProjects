9f3daf55c5fcd40a2d71abfa0cd7b8cf2ed43ed7
Gaolbirdy <gaolbird@yeah.net>
2017-11-04 09:14:40 +0800

    commit

---
+++ Line Renderer/Assets/Scripts/Test1.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test1 : MonoBehaviour
+ {
+    private LineRenderer lineRenderer;
+    private Vector3 position;
+    private int index = 0;
+    private int LengthOfLineRenderer = 0;
+
+    private void Start()
+    {
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.material = new Material(Shader.Find("Particles/Additive"));
+        lineRenderer.startColor = Color.red;
+        lineRenderer.endColor = Color.yellow;
+        lineRenderer.startWidth = 0.02f;
+        lineRenderer.endWidth = 0.1f;
+    }
+
+    private void Update()
+    {
+        //lineRenderer = GetComponent<LineRenderer>();
+        if(Input.GetMouseButton(0))
+        {
+            position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z + 1));
+            LengthOfLineRenderer++;
+            lineRenderer.positionCount = LengthOfLineRenderer;
+        }
+        while (index < LengthOfLineRenderer)
+        {
+            lineRenderer.SetPosition(index, position);
+            index++;
+        }
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("当前鼠标X轴位置：" + Input.mousePosition.x);
+        GUILayout.Label("当前鼠标Y轴位置：" + Input.mousePosition.y);
+        GUILayout.Label("当前鼠标Z轴位置：" + Input.mousePosition.z);
+        GUILayout.Label("线段X轴位置：" + position.x);
+        GUILayout.Label("线段Y轴位置：" + position.y);
+        GUILayout.Label("线段Z轴位置：" + position.z);
+        GUILayout.Label("lineRenderer.positionCount：" + lineRenderer.positionCount);
+    }
+}
---
+++ Line Renderer/Assets/Scripts/Test2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test2 : MonoBehaviour
+ {
+    private GameObject LineRendererGameObject;
+    private LineRenderer lineRenderer;
+    private int lineLength = 4;
+    private Vector3 v0 = new Vector3(1, 0, 0);
+    private Vector3 v1 = new Vector3(0, 1, 0);
+    private Vector3 v2 = new Vector3(0, 0, 1);
+    private Vector3 v3 = new Vector3(1, 0, 0);
+
+    private void Start () 
+	{
+        LineRendererGameObject = GameObject.Find("Test2");
+        lineRenderer = LineRendererGameObject.GetComponent<LineRenderer>();
+        lineRenderer.positionCount = lineLength;
+        lineRenderer.SetPosition(0, v0);
+        lineRenderer.SetPosition(1, v1);
+        lineRenderer.SetPosition(2, v2);
+        lineRenderer.SetPosition(3, v3);
+    }
+
+    private void Update () 
+	{
+        //lineRenderer.SetPosition(0, v0);
+        //lineRenderer.SetPosition(1, v1);
+        //lineRenderer.SetPosition(2, v2);
+        //lineRenderer.SetPosition(3, v3);
+    }
+}
