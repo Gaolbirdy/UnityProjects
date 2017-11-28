cdbd811a69f123a70d0635c1e41fce499c015349
liukuo <liukuo@gitlab.com>
2017-11-29 06:58:23 +0800

    commit

---
+++ UnityAPI/Assets/API01EventFunction.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class API01EventFunction : MonoBehaviour 
+{
+    private void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    private void Update()
+    {
+        Debug.Log("Update");
+    }
+
+    private void Reset()
+    {
+        Debug.Log("Reset");
+    }
+}
