b4911cfe80a9d5191ed1a5ccadbe56d9d485c625
liukuo <liukuo@gitlab.com>
2017-12-01 08:32:45 +0800

    commit

---
+++ UnityAPI/Assets/Scripts/API04Message.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class API04Message : MonoBehaviour 
+{
+    public GameObject target;
+
+    private void Start()
+    {
+        //target.BroadcastMessage("Attack", null, SendMessageOptions.DontRequireReceiver);
+        //target.SendMessage("Attack", null, SendMessageOptions.DontRequireReceiver);
+        target.SendMessageUpwards("Attack", null, SendMessageOptions.DontRequireReceiver);
+    }
+
+}
---
+++ UnityAPI/Assets/Scripts/API06MonoBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class API06MonoBehaviour : MonoBehaviour 
+{
+    public Cube cube;
+
+    private void Start()
+    {
+        Debug.Log(this.isActiveAndEnabled);
+        Debug.Log(this.enabled);
+        enabled = false;
+        Debug.Log(name);
+        Debug.Log(tag);
+        Debug.Log(gameObject);
+        Debug.Log(transform);
+        Debug.Log(this);
+
+        print("haha");
+
+        Debug.Log(cube.isActiveAndEnabled);
+        Debug.Log(cube.enabled);
+        cube.enabled = false;
+        Debug.Log(cube.name);
+        Debug.Log(cube.tag);
+        Debug.Log(cube.gameObject);
+        Debug.Log(cube.transform);
+        Debug.Log(cube);
+
+    }
+}
