6c7778cfcb6f345ff2282ecc28f0c180e5d77116
liukuo <liukuo@gitlab.com>
2017-12-04 09:43:20 +0800

    commit

---
+++ UnityAPI/Assets/Scripts/API07Invoke.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class API07Invoke : MonoBehaviour 
+{
+    private void Start()
+    {
+        //Invoke("Attack", 3f);
+        InvokeRepeating("Attack", 4f, 2f);
+        CancelInvoke("Attack");
+    }
+
+    private void Update()
+    {
+        bool res = IsInvoking("Attack");
+        print(res);
+    }
+
+    void Attack()
+    {
+        print("攻击目标");
+    }
+}
---
+++ UnityAPI/Assets/Scripts/API08Coroutine.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class API08Coroutine : MonoBehaviour 
+{
+    public GameObject cube;
+
+    private void Start()
+    {
+        //print("haha");
+        ////ChangerColor();
+        //StartCoroutine(ChangerColor());
+        //// 协程方法开启后，会继续运行下面的代码，不会等协程方法运行结束才继续执行
+        //print("hahaha");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(Fade());
+        }
+    }
+
+    IEnumerator Fade()
+    {
+        for (; ; )
+        {
+            //cube.GetComponent<MeshRenderer>().material.color = new Color(i, i, i, i);
+            Color color = cube.GetComponent<MeshRenderer>().material.color;
+            Color newColor = Color.Lerp(color, Color.red, 0.02f);
+            //newColor = color;??
+            cube.GetComponent<MeshRenderer>().material.color = newColor;
+            yield return new WaitForSeconds(0.02f);
+            if (Mathf.Abs(Color.red.g - newColor.g) <= 0.01f)
+            {
+                break;
+            }
+        }
+    }
+
+    // Coroutines
+    // 1，返回值是IEnumerator
+    // 2，返回参数的时候使用yield return null / 0
+    // 3，协程方法的调用StartCoroutine(Method())
+    IEnumerator ChangerColor()
+    {
+        print("hahaColor");
+        yield return new WaitForSeconds(3);
+        for (int i = 0; i < 1; i++)
+        {
+            cube.GetComponent<MeshRenderer>().material.color = Color.blue;
+        }
+        print("hahaColor");
+        yield return null;
+    }
+}
