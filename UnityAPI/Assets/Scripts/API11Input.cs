da5586e6798a2f1427299957c7a80f742d52bbec
liukuo <liukuo@gitlab.com>
2017-12-07 08:38:34 +0800

    commit

--- UnityAPI/Assets/Scripts/API11Input.cs
+++ UnityAPI/Assets/Scripts/API11Input.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class API11Input : MonoBehaviour 
 {
+    public Transform cube;
+
     private void Start()
     {
         
@@ -11,17 +13,63 @@ public class API11Input : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            print("KeyDown");
-        }
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            print("KeyUp");
-        }
-        if (Input.GetKey(KeyCode.Space))
-        {
-            print("Key");
-        }
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    print("KeyDown");
+        //}
+        //if (Input.GetKeyUp(KeyCode.Space))
+        //{
+        //    print("KeyUp");
+        //}
+        //if (Input.GetKey(KeyCode.Space))
+        //{
+        //    print("Key");
+        //}
+        //if (Input.GetKeyDown("left shift"))
+        //{
+        //    print("left shift");
+        //}
+
+        //if (Input.GetMouseButton(0))
+        //    Debug.Log("left click");
+
+        //if (Input.GetMouseButton(1))
+        //    Debug.Log("right click");
+
+        //if (Input.GetMouseButton(2))
+        //    Debug.Log("middle click");
+
+        //if (Input.GetMouseButtonDown(0))
+        //    Debug.Log("left click");
+
+        //if (Input.GetMouseButtonDown(1))
+        //    Debug.Log("right click");
+
+        //if (Input.GetMouseButtonDown(2))
+        //    Debug.Log("middle click");
+
+        //if (Input.GetButton("Fire1"))
+        //    Debug.Log("Fire1 Button");
+
+        //if (Input.GetButtonDown("Fire1"))
+        //    Debug.Log("Fire1 Down");
+
+        //if (Input.GetButtonUp("Fire1"))
+        //    Debug.Log("Fire1 Up");
+
+        //if (Input.GetButtonDown("Horizontal"))
+        //    print("Horizontal Down");
+
+        //print(Input.GetAxisRaw("Horizontal"));
+
+        //cube.Translate(Vector3.right * Time.deltaTime * Input.GetAxisRaw("Horizontal") * 5);
+        //cube.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal") * 5);
+
+        //if (Input.anyKeyDown)
+        //    print("anyKeyDown");
+
+        //print(Input.mousePosition);
+
+
     }
 }
---
+++ UnityAPI/Assets/Scripts/API12Vector2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class API12Vector2 : MonoBehaviour 
+{
+    private void Start()
+    {
+        print(Vector2.down);
+        print(Vector2.up);
+        print(Vector2.left);
+        print(Vector2.right);
+        print(Vector2.one);
+        print(Vector2.zero);
+
+        Vector2 a = new Vector2(2, 2);
+        Vector2 b = new Vector2(3, 4);
+        print(a.magnitude);
+        print(a.sqrMagnitude);
+        print(b.magnitude);
+        print(b.sqrMagnitude);
+
+        print(a.normalized);
+        print(b.normalized);
+
+        print(a.x + "," + a.y);
+        a.Normalize();
+        print(a[0] + "," + a[1]);
+    }
+}
