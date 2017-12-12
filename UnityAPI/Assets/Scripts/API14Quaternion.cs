80cbb92e64badb1a604dc67e94fb7fd17b549488
liukuo <liukuo@gitlab.com>
2017-12-12 08:31:05 +0800

    commit

--- UnityAPI/Assets/Scripts/API14Quaternion.cs
+++ UnityAPI/Assets/Scripts/API14Quaternion.cs
@@ -6,16 +6,33 @@ public class API14Quaternion : MonoBehaviour
 {
     public Transform cube;
 
+    public Transform player;
+    public Transform enemy;
+
     private void Start()
     {
-        print(cube.eulerAngles);
-        print(cube.localEulerAngles);
-        print(cube.rotation);
-        print(cube.localRotation);
+        //print(cube.eulerAngles);
+        //print(cube.localEulerAngles);
+        //print(cube.rotation);
+        //print(cube.localRotation);
+
+        //cube.eulerAngles = new Vector3(45, 45, 45);
+        //cube.rotation = Quaternion.Euler(new Vector3(45, 45, 45));
+        //print(cube.rotation.eulerAngles);
+        
     }
 
     private void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space))    // 按键down后，自己完成旋转
+        {
+            Vector3 dir = enemy.position - player.position;
+            //print(dir);
+            dir.y = 0;
+            //print(dir);
+            Quaternion target = Quaternion.LookRotation(dir);
+            player.rotation = Quaternion.Lerp(player.rotation, target, Time.deltaTime); // 中间变量lerp
+            //player.rotation = Quaternion.Slerp(player.rotation, target, Time.deltaTime);
+        }
     }
 }
