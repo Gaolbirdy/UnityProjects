022478105b0348b75afc5b72851b4087685d72f9
liukuo <liukuo@gitlab.com>
2017-12-14 08:40:00 +0800

    commit

--- UnityAPI/Assets/Scripts/API16Camera.cs
+++ UnityAPI/Assets/Scripts/API16Camera.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class API16Camera : MonoBehaviour 
 {
-    private Camera c;
+    private Camera mainCamera;
 
     private void Start()
     {
-        c = GameObject.Find("Main Camera").GetComponent<Camera>();
+        //c = GameObject.Find("Main Camera").GetComponent<Camera>();
+        mainCamera = Camera.main;
     }
 
     private void Update()
     {
-        
+        //Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        //RaycastHit hit;
+        //bool isCollider = Physics.Raycast(ray, out hit);
+        //if (isCollider)
+        //{
+        //    Debug.Log(hit.collider);
+        //}
+
+        //Ray ray = mainCamera.ScreenPointToRay(new Vector3(200, 200, 0));
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction * 100, Color.yellow);
     }
 }
