0d3a863dc7b6bf6ff233dccabe1f03ff900e9d40
liukuo <liukuo@gitlab.com>
2018-01-08 08:48:38 +0800

    commit

---
+++ UnityAPI/Assets/Scripts/API18Application.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class API18Application : MonoBehaviour 
+{
+    private void Start()
+    {
+        print(Application.identifier);
+        print(Application.companyName);
+        print(Application.productName);
+        print(Application.installerName);
+        print(Application.installMode);
+        print(Application.isEditor);
+        print(Application.isFocused);
+        print(Application.isMobilePlatform);
+        print(Application.isPlaying);
+        print(Application.isWebPlayer);
+        print(Application.platform);
+        print(Application.unityVersion);
+        print(Application.version);
+        print(Application.runInBackground);
+
+        Application.Quit();
+        Application.OpenURL("www.sikiedu.com");
+        //Application.CaptureScreenshot("游戏截图");
+        //ScreenCapture.CaptureScreenshot("游戏截图.png");
+
+        //Application.LoadLevel(1);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Application.isPlaying = false;
+            //UnityEditor.EditorApplication.isPlaying = false;
+            SceneManager.LoadScene(1);
+        }
+    }
+}
---
+++ UnityAPI/Assets/Scripts/API19SceneManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class API19SceneManager : MonoBehaviour 
+{
+    private void Start()
+    {
+        print(SceneManager.sceneCount);
+        print(SceneManager.sceneCountInBuildSettings);
+
+        print(SceneManager.GetActiveScene().name);
+        print(SceneManager.GetSceneAt(0).name);
+
+        SceneManager.activeSceneChanged += OnActiveSceneChanged;
+        SceneManager.sceneLoaded += OnSceneLoaded;z
+    }
+
+    void OnActiveSceneChanged(Scene a, Scene b)
+    {
+        print(a.name);
+        print(b.name);
+    }
+
+    void OnSceneLoaded(Scene a, LoadSceneMode mode)
+    {
+        print(a.name + " " + mode);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(1);
+            //SceneManager.LoadScene(SceneManager.GetSceneByBuildIndex(1).name);
+        }
+    }
+}
